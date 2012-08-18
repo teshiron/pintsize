7b4640464ba3935b05ef0881d18bbb46b8110ab0
Ryan <evendell@gmail.com>
2012-08-17 23:31:07 -0700

    Fixed nickserv identify for rizon.net. Oops.

--- src/Irc.pl
+++ src/Irc.pl
@@ -450,8 +450,8 @@ sub sparse {
         &nickServ($1);		# added by the xk.
     } elsif (/^:NickServ!NickServ\@services\. NOTICE \S+ :(.*)/i) {
         &nickServ($1);      # freenode style
-    } elsif (/^:NickServ!services@rizon\. NOTICE \S+ :(.*)/i) {
-	&nickserv($1);
+    } elsif (/^:NickServ!service\@rizon\.net NOTICE \S+ :(.*)/i) {
+	    &nickServ($1);
     } elsif (/^:ChanServ!s\@ChanServ NOTICE \S+ :(.*)/i) {
         &chanServ($1);		# added by the xk.
     } elsif (/^:(\S+)!(\S+)@(\S+)\s(PRIVMSG|NOTICE)\s([\#\&]?\S+)\s:(.*)/) {
